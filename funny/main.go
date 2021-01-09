383b8e65f1a78e7c4579712367490b60ca8343fc
Yutolab1120 <75582579+Yutolab1120@users.noreply.github.com>
2021-01-09 22:34:31 +0900

    Add files via upload

---
+++ funny/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	
+	fs := http.FileServer(http.Dir("public"))
+	http.Handle("/", fs)
+
+	log.Println("Listen to 'http://localhost:3000'...")
+
+	http.ListenAndServe(":3000", nil)
+
+	
+}
